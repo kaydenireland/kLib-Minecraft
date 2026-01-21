264bdfe98acac1be92e13d891f9209d23ab9cc11
kaydenireland <kaydenireland@aol.com>
2026-01-21 17:27:07 -0500

    CooldownItem NBT Data

--- src/main/java/net/kallen/klib/item/CoolDownItem.java
+++ src/main/java/net/kallen/klib/item/CoolDownItem.java
@@ -1,8 +1,11 @@
 package net.kallen.klib.item;
 
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 public class CoolDownItem extends Item {
     public CoolDownItem(Properties pProperties) {
@@ -10,7 +13,25 @@ public class CoolDownItem extends Item {
     }
 
 
-    public void applyCooldown(Player pPlayer, ItemStack stack, int coolDownTime){
-        pPlayer.getCooldowns().addCooldown(stack, coolDownTime);
+    public InteractionResult applyCooldown(Level pLevel, Player pPlayer, ItemStack stack, int coolDownTime, String tag){
+
+        if (!pLevel.isClientSide()) {
+            CompoundTag persistentData = pPlayer.getPersistentData();
+
+            long lastUsedTime = persistentData.getLongOr(tag, 0);
+            long currentTime = pPlayer.level().getGameTime();
+
+            if ((currentTime < lastUsedTime + coolDownTime) && lastUsedTime > 0) {
+                return InteractionResult.FAIL;
+            }
+
+            persistentData.putLong(tag, currentTime);
+            pPlayer.getCooldowns().addCooldown(stack, coolDownTime);
+
+            return InteractionResult.SUCCESS;
+        }
+
+        return InteractionResult.PASS;
+
     }
 }
